8990092d4c97de420ec52becc51c94afa3c940ec
Rita0702 <0702.t.kawakami@gmail.com>
2019-09-17 17:18:11 +0900

    re-edit2

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,8 +1,6 @@
 class Api::MessagesController < ApplicationController
   def index
     group = Group.find(params[:group_id])
-    last_message_id = params[:id].to_i
-    @messages = group.messages.includes(:user).where("id > #{last_message_id}")
-    render json: @messages
+    @messages = group.messages.includes(:user).where("id > #{params[:id].to_i}")
   end
 end
